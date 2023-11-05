bd5831e3579c1c0f9c47bfe70b4dc04b22b92d5d
sebastian <sebastianseberianodev@gmail.com>
2023-11-05 12:30:47 -0600

    version mobile done

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+let btnMenu = document.querySelector(".btn__menu img");
+let menuHeader = document.querySelector(".header__menu");
+let links = document.querySelectorAll(".link__item");
+
+let clicks = 0;
+
+function toggleMenu(){
+    menuHeader.classList.toggle("active");
+    clicks = clicks + 1;
+
+    if (clicks % 2 != 0) {
+        btnMenu.setAttribute("src", "../../images/icon-close.svg");
+    }else{
+        btnMenu.setAttribute("src", "../../images/icon-hamburger.svg");
+    }
+
+}
+
+btnMenu.addEventListener("click",toggleMenu);
+
+links.forEach((linkItem)=>{
+    linkItem.addEventListener("click",toggleMenu);
+});
